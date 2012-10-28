c80e9293b187014e0286e7c9fb154b690e88d4f1
Tong-Kiat Tan <2617095+tongkiat@users.noreply.github.com>
2012-10-28 20:10:31 +0800

    Add 01-toys.scm, p. 3

--- 01-toys.scm
+++ 01-toys.scm
@@ -1,3 +1,30 @@
 ;;;
 ;;; (01-toys.scm)
 ;;;
+
+;; ... p. 10
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x))
+         (not (null? x)))))
+
+;; p. 3
+
+(atom? (quote atom))                    ; => #t
+
+(atom? 'atom)                           ; => #t
+
+(atom? 'turkey)                         ; => #t
+
+(atom? 1492)                            ; => #t
+
+(atom? 'u)                              ; => #t
+
+(atom? '*abc$)                          ; => #t
+
+(list? (quote (atom)))                  ; => #t
+
+(list? '(atom))                         ; => #t
+
+(list? '(atom turkey or))               ; => #t
